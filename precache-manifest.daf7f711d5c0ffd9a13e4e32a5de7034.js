2b4f626ebce11303f679b1e56a9ac50bbc1cc50f
mxuan1998 <mxuan1990@gmail.com>
2020-05-13 19:43:22 +0400

    Updates

---
+++ precache-manifest.daf7f711d5c0ffd9a13e4e32a5de7034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291f16c76fcefde179a46593ddba371c",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "464a31798bd1918b8881",
+    "url": "/react-tutorial/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "ee1c81a9a2fd61300c6e",
+    "url": "/react-tutorial/static/js/2.9f538a2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.9f538a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464a31798bd1918b8881",
+    "url": "/react-tutorial/static/js/main.d2cab9ed.chunk.js"
+  },
+  {
+    "revision": "8526c682e0d87d451f0e",
+    "url": "/react-tutorial/static/js/runtime-main.5e5f67a8.js"
+  }
+]);
